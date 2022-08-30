45d840b82df5df63fceded3c1c2d1f3766fdda57
Jheorgenes Warlley Dias da Silva <jheorgenes@gmail.com>
2022-08-30 17:27:36 -0300

    [30-08-2022]  - Criando o componente de Cadastro de Técnico (Ainda incompleto)

--- src/app/views/components/tecnico/tecnico-read/tecnico-read.component.ts
+++ src/app/views/components/tecnico/tecnico-read/tecnico-read.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { Tecnico } from 'src/app/models/tecnico';
 import { TecnicoService } from 'src/app/services/tecnico.service';
 
@@ -11,14 +12,16 @@ import { TecnicoService } from 'src/app/services/tecnico.service';
 })
 export class TecnicoReadComponent implements AfterViewInit {
 
-  tecnicos: Tecnico[]= []; //Lista de técnicos vazia
+  tecnicos: Tecnico[] = []; //Lista de técnicos vazia
 
   displayedColumns: string[] = ['id', 'nome', 'cpf', 'telefone']; 
   dataSource = new MatTableDataSource<Tecnico>(this.tecnicos); //Componente de Tabela, recebendo uma lista de Técnicos do tipo Técnico
 
   @ViewChild(MatPaginator) paginator!: MatPaginator; //Componente de paginação injetado como atributo
 
-  constructor(private service: TecnicoService){}
+  constructor(
+    private service: TecnicoService,
+    private router : Router ){}
 
   /* Antes de inicializar a visualização, executa esse método abaixo */
   ngAfterViewInit() {
@@ -31,5 +34,9 @@ export class TecnicoReadComponent implements AfterViewInit {
       this.dataSource = new MatTableDataSource<Tecnico>(this.tecnicos); //Instancia o dataSource no tipo Tecnico
       this.dataSource.paginator = this.paginator;
     })
+  }
+
+  navigateToCreate(): void {
+    this.router.navigate(['tecnicos/create'])
   }
 }

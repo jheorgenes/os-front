2b6ba4a728d5e435b74c6c826cf45f59a002e7a9
Jheorgenes Warlley Dias da Silva <jheorgenes@gmail.com>
2022-08-31 17:47:53 -0300

    [31-08-2022]  - Continuando o desenvolvimento do method Post de criação de um técnico.

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,7 +10,16 @@ import { Router } from '@angular/router';
 })
 export class TecnicoCreateComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  tecnico: Tecnico = {
+    id: '',
+    nome: 'Agnaldo',
+    cpf: '299.844.540-37',
+    telefone: '(00) 90000-0000'
+  }
+
+  constructor(
+    private router : Router,
+    private service: TecnicoService) { }
 
   ngOnInit(): void {
   }
@@ -17,4 +28,14 @@ export class TecnicoCreateComponent implements OnInit {
     this.router.navigate(['tecnicos'])
   }
 
+  create(): void {
+    this.service.create(this.tecnico).subscribe((resposta) => {
+      this.router.navigate(['tecnicos'])
+      this.service.message('Tecnico criado com sucesso');
+    }, err => {
+      if(err.error.error.match('já cadastrado')){
+        this.service.message(err.error.error);
+      }
+    })
+  }
 }

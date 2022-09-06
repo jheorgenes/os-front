7a22078a7f34e11dc6898d23bfe2f41a6c5cdd66
Jheorgenes Warlley Dias da Silva <jheorgenes@gmail.com>
2022-09-06 17:53:45 -0300

    [06-09-2022]  - Incrementando o method delete de um técnico (Ainda Incompleto)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './views/components/home/home.component';
 import { TecnicoUpdateComponent } from './views/components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoCreateComponent } from './views/components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
+import { TecnicoDeleteComponent } from './views/components/tecnico/tecnico-delete/tecnico-delete.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'tecnicos/update/:id',
     component: TecnicoUpdateComponent
+  },
+  {
+    path: 'tecnicos/delete/:id',
+    component: TecnicoDeleteComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { HomeComponent } from './views/components/home/home.component';
 import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
 import { TecnicoCreateComponent } from './views/components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoUpdateComponent } from './views/components/tecnico/tecnico-update/tecnico-update.component';
+import { TecnicoDeleteComponent } from './views/components/tecnico/tecnico-delete/tecnico-delete.component';
 
 
 
@@ -39,6 +40,7 @@ import { TecnicoUpdateComponent } from './views/components/tecnico/tecnico-updat
     TecnicoReadComponent,
     TecnicoCreateComponent,
     TecnicoUpdateComponent,
+    TecnicoDeleteComponent,
   ],
   imports: [
     BrowserModule,

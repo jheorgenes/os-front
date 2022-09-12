1f1e24e7b428cf1c6207b2593ff58186a629f38d
Jheorgenes Warlley Dias da Silva <jheorgenes@gmail.com>
2022-09-12 17:48:32 -0300

    [12-09-2022]   - Criando o método de Deletar Técnicos   - Finalizado o CRUD completo de Técnicos   - Já realizado validações em técnicos

--- src/app/views/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ src/app/views/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -25,13 +25,24 @@ export class TecnicoDeleteComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.id_tec = this.route.snapshot.paramMap.get('id')!
-    this.findById();
+    this.id_tec = this.route.snapshot.paramMap.get('id')! //Obtendo o id da rota e passando para a variável
+    this.findById(); //Efetuando a busca do técnico no banco com o id que foi obtido.
   }
 
   findById(): void {
-    this.service.findById(this.id_tec).subscribe(resposta => {
+    this.service.findById(this.id_tec).subscribe(resposta => { //A resposta retorna um técnico que será preenchido no objeto técnico
       this.tecnico = resposta;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.id_tec).subscribe(resposta => {
+      this.router.navigate(['tecnicos']);
+      this.service.message('Técnico deletado com sucesso!');
+    }, err => {
+      if(err.error.error.match('possui Ordens de Serviço')){
+        this.service.message(err.error.error);
+      }
     })
   }
 
